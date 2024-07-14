d242bf07bd5262ed7a8cdc77eb655237d96f82cf
hsjni0110 <hansukjinid@gmail.com>
2024-07-14 11:58:00 +0900

    init : init project

---
+++ src/main/java/com/backend/filb/FilbApplication.java
@@ -0,0 +1,13 @@
+package com.backend.filb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilbApplication.class, args);
+    }
+
+}
