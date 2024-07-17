a912b5e43340da2ce14ab8d096856b9f06b59959
alsswl <alswl4223@naver.com>
2024-07-18 02:14:33 +0900

    feat : 로그인 service 추가

--- src/main/java/com/backend/filb/application/MemberService.java
+++ src/main/java/com/backend/filb/application/MemberService.java
@@ -1,6 +1,5 @@
 package com.backend.filb.application;
 
-
 import com.backend.filb.domain.entity.Member;
 import com.backend.filb.domain.repository.MemberRepository;
 import com.backend.filb.dto.MemberRequest;
@@ -12,9 +11,11 @@ import java.util.NoSuchElementException;
 public class MemberService {
 
     private MemberRepository memberRepository;
+    private JwtService jwtService;
 
-    public MemberService(MemberRepository memberRepository){
+    public MemberService(MemberRepository memberRepository,JwtService jwtService){
         this.memberRepository = memberRepository;
+        this.jwtService = jwtService;
     }
 
     public Member join(MemberRequest memberRequest) {
@@ -23,6 +24,16 @@ public class MemberService {
             memberRepository.save(member);
         }
         throw new NoSuchElementException("이미 존재하는 회원입니다.");
+    }
+
+    public String login(MemberRequest memberRequest) {
+        Member dbMember = memberRepository.findById(memberRequest.memberId())
+                .orElseThrow(() -> new NoSuchElementException("로그인에 실패했습니다 다시 시도해주세요"));
+        if (!memberRequest.password().equals(dbMember.getPassword())) {
+            throw new NoSuchElementException("로그인에 실패하였습니다. 다시 시도해주세요");
+        } else {
+            return jwtService.createJWT(memberRequest.memberId());
+        }
     }
 
     private Member MapMemberRequestToMember(MemberRequest memberRequest){
--- src/main/java/com/backend/filb/domain/entity/Member.java
+++ src/main/java/com/backend/filb/domain/entity/Member.java
@@ -24,4 +24,7 @@ public class Member {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
 }
