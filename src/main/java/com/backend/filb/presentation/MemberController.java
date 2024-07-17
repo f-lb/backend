25362308f979093063db74d1db299328dfc94e64
alsswl <alswl4223@naver.com>
2024-07-18 02:13:56 +0900

    feat : MemberController 에 회원가입 기능 추가

--- src/main/java/com/backend/filb/presentation/MemberController.java
+++ src/main/java/com/backend/filb/presentation/MemberController.java
@@ -3,6 +3,7 @@ package com.backend.filb.presentation;
 import com.backend.filb.application.MemberService;
 import com.backend.filb.domain.entity.Member;
 import com.backend.filb.dto.MemberRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +17,22 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<Member> join(
+    public ResponseEntity<String> join(
             @RequestBody MemberRequest memberRequest
     ) {
-        Member member = memberService.join(memberRequest);
-        return ResponseEntity.ok().body(member);
+        memberService.join(memberRequest);
+        return ResponseEntity.ok().body("회원가입에 성공하셨습니다.");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @RequestBody MemberRequest memberRequest
+    ){
+        String jwt = memberService.login(memberRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", jwt);
+        return ResponseEntity.ok().headers(headers).body("로그인에 성공하셨습니다");
+    }
+
+
 }
