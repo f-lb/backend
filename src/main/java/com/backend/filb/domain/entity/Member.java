0f58eda32e3f0499457c5f465455661890c6a170
alsswl <alswl4223@naver.com>
2024-07-17 16:12:25 +0900

    feat : member 엔티티 생성

--- src/main/java/com/backend/filb/domain/entity/Member.java
+++ src/main/java/com/backend/filb/domain/entity/Member.java
@@ -1,4 +1,19 @@
 package com.backend.filb.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
 }
