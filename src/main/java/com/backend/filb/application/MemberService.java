7b5bcef1b6c1bc342f538aecbac708187df82526
alsswl <alswl4223@naver.com>
2024-07-21 23:42:40 +0900

    refactor : 불필요한 import 제거

--- src/main/java/com/backend/filb/application/MemberService.java
+++ src/main/java/com/backend/filb/application/MemberService.java
@@ -7,7 +7,6 @@ import com.backend.filb.dto.MemberRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
--- src/main/java/com/backend/filb/dto/MemberRequest.java
+++ src/main/java/com/backend/filb/dto/MemberRequest.java
@@ -1,9 +1,5 @@
 package com.backend.filb.dto;
 
-import com.backend.filb.domain.entity.Diary;
-
-import java.util.List;
-
 public record MemberRequest(
         String email,
         String password,
