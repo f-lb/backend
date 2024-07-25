b8692352e8284909c0122e4c3a5bbfc3a3dd4631
alsswl <alswl4223@naver.com>
2024-07-26 01:25:52 +0900

    feat : diary에 report 연결

--- src/main/java/com/backend/filb/domain/entity/Diary.java
+++ src/main/java/com/backend/filb/domain/entity/Diary.java
@@ -11,17 +11,26 @@ public class Diary {
     Long diaryId;
     @Column(nullable = false)
     Date createdDate;
-    @Column(nullable = false)
+    @Column(nullable = false,length = 1000)
     String content;
 
-    public Diary(Long diaryId, String content, Date createdDate) {
+    @OneToOne
+    Report report;
+
+    public Diary(Long diaryId, String content, Date createdDate,Report report) {
         this.diaryId = diaryId;
         this.content = content;
         this.createdDate = createdDate;
+        this.report = report;
     }
 
     public Diary() {
 
+    }
+
+
+    public void setReport(Report report) {
+        this.report = report;
     }
 
     public Long getDiaryId() {
@@ -34,5 +43,9 @@ public class Diary {
 
     public String getContent() {
         return content;
+    }
+
+    public Report getReport() {
+        return report;
     }
 }
